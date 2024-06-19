27b36d64a31d6831e94d37b2be37876915347834
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-06-18 21:55:12 -0400

    Added check win functionality

--- game.js
+++ game.js
@@ -38,11 +38,61 @@ const displayBoard = () => {
         cell.setAttribute("grid-num", i);
         cell.classList.add("cell");
 
+         // Add a click event listener to each cell.
         cell.addEventListener("click", () => handleCellClick(i));
 
+        // Display the player's letter if the cell is not empty.
         cell.textContent = board[i];
         boardGrid.append(cell);
     }
+};
+
+const handleCellClick = (index) => {
+    if(gameOver) return;
+
+    const board = boardObject.getBoard;
+
+    //Checking if the cell is empty
+    if(board[index] === "") {
+        //Update the board with the currPlayer's letter
+        board[index] = currPlayer.letter;
+        boardObject.setBoard(board);
+        
+        //Update interface with new board
+        displayBoard();
+
+    }
+};
+
+const checkWin = (letter) => {
+    const board = boardObject.getBoard;
+
+    const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8], // Rows
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8], // Columns
+        [0, 4, 8],
+        [2, 4, 6], // Diagonals
+    ];
+    
+    //check for winning combo
+    for (const combo of winCombos) {
+        const [cellOne, cellTwo, cellThree] = combo;
+
+        if(board[cellOne] === letter && board[cellTwo] === letter && board[cellThree] === letter) {
+            const winningCells = document.querySelectorAll(`grid-num=${cellOne}, grid-num=${cellTwo}, grid-num=${cellThree}`);
+            console.log(winningCells);
+            winningCells.forEach((cell) => {
+                cell.classList.add("winning-cell");
+            });
+
+            return true; // Player has won.
+        }
+    }
+    return false; //No winning combo found
 };
 restartButton.addEventListener("click", () => {
     boardObject.resetBoard();
