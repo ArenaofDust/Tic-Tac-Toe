4b7f8c6c61cea23b870d4147a02e005e6cc0854f
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-06-18 21:05:17 -0400

    Added displayGrid function

--- game.js
+++ game.js
@@ -13,24 +13,44 @@ const gameBoard = () => {
 
 };
 
-const player = (user) => {
-    return {user};
+const player = (letter) => {
+    return {letter};
 };
 
 const boardObject = gameBoard();
 const playerOne = player("X");
 const playerTwo = player("O");
 
-const gameInfo = document.querySelector(".game-info");
-const boardGrid = document.querySelector(".board-container");
+const gameInfo = document.querySelector(".player-info");
+const boardGrid = document.querySelector(".game-board");
 const restartButton = document.querySelector(".restart-button");
 
 let gameOver = false;
 let currPlayer = playerOne;
 
+const displayBoard = () => {
+    const board = boardObject.getBoard();
+    boardGrid.innerHtml= "";
+    gameInfo.textContent = `Player ${currPlayer.letter}'s Turn!`;
+
+    for (let i = 0; i < board.length; i++) {
+        const cell = document.createElement("div");
+        cell.setAttribute("grid-num", i);
+        cell.classList.add("cell");
+
+        cell.addEventListener("click", () => handleCellClick(i));
+
+        cell.textContent = board[i];
+        boardGrid.append(cell);
+    }
+};
 restartButton.addEventListener("click", () => {
     boardObject.resetBoard();
-    boardGrid.innerHtml= ""
+    gameOver = false;
+    currPlayer = playerOne;
+    displayBoard();
 
 });
+
+displayBoard();
 
